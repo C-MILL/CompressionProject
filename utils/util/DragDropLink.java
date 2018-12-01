afe18647e5cad220631a11e679b9b6f680af88f6
atorha <chrigu_m@hotmail.com>
2018-12-01 16:43:31 +0100

    Hier nochmals dasselbe. Aber es ist nun eine Methode die man aufrufen kann. Ausserdem gibt es ein String Array zurück welche alle Links enthält für die einzelnen Fotos.

--- utils/util/DragDropLink.java
+++ utils/util/DragDropLink.java
@@ -2,25 +2,45 @@ package util;
 
 	import javafx.application.Application;
 	import javafx.event.EventHandler;
-	import javafx.scene.Scene;
-	import javafx.scene.control.Label;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 	import javafx.scene.input.DragEvent;
 	import javafx.scene.input.Dragboard;
 	import javafx.scene.input.TransferMode;
 	import javafx.scene.layout.StackPane;
 	import javafx.scene.layout.VBox;
-	import javafx.stage.Stage;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
-	public class DragDropLink extends Application {
-
-	    public void start(Stage primaryStage) {
-	        Label label = new Label("Drag your Files here.");
-	        Label dropped = new Label("This has changed again");
-	        VBox dragTarget = new VBox();
-	        dragTarget.getChildren().addAll(label,dropped);
-	        dragTarget.setOnDragOver(new EventHandler<DragEvent>() {
+	public class DragDropLink {
+		
+		static String[] links=null;
+		public static String[] display(String abbrechen) {
+			
+			Stage window = new Stage();
+			
+			window.initModality(Modality.APPLICATION_MODAL);
+			window.setTitle("Komprimieren");
+			window.setMinWidth(250);
+			
+			Label label = new Label();
+			label.setText("Ziehe alle Dateien die du komprimieren willst in dieses Fenster.");
+			
+			Button closeButton = new Button("Abbrechen");
+			closeButton.setOnAction(e->window.close());
+			
+		
+			VBox layout = new VBox(10);
+			layout.getChildren().addAll(label,closeButton);
+			layout.setAlignment(Pos.CENTER);
+			
+			
+		    //Dies ist die DragOver Methode. Sie Schaut ob etwas ins Feld geschoben wird. Sie macht eigentlich nur dass man überhaupt etwas droppen kann
+			layout.setOnDragOver(new EventHandler<DragEvent>() {
 	            public void handle(DragEvent event) {
-	                if (event.getGestureSource() != dragTarget
+	                if (event.getGestureSource() != label
 	                        && event.getDragboard().hasFiles()) {
 	                    //TransferMode steuert wie dass Dateien abgelegt weden können. Hier kann man die Bilder reinkopieren und in das Feld kopieren.
 	                    event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
@@ -28,41 +48,38 @@ package util;
 	                event.consume();
 	            }
 	        });
+			
+		      //Diese Methode reagiert wenn tatsächlich etwas gedropped wird.
+			  label.setOnDragDropped(new EventHandler<DragEvent>() {
 
-	        dragTarget.setOnDragDropped(new EventHandler<DragEvent>() {
+		            @Override
+		            public void handle(DragEvent event) {
+		                Dragboard db = event.getDragboard();
+		                boolean success = false;
+		                if (db.hasFiles()) {
+		                String link=db.getFiles().toString();
+		                links = link.split(","); 
+		       			success = true;
+		                }
+		        
+		                event.setDropCompleted(success);
 
-	            @Override
-	            public void handle(DragEvent event) {
-	                Dragboard db = event.getDragboard();
-	                boolean success = false;
-	                if (db.hasFiles()) {
-	                    dropped.setText(db.getFiles().toString());
-	                    success = true;
-	                }
-	                //sieht nach ob daten gedropped wurden
-	                event.setDropCompleted(success);
+		                event.consume();
 
-	                event.consume();
-	            }
-	        });
+		            }
+		        });
+			
+			Scene scene = new Scene(layout);
+			window.setScene(scene);
+			window.showAndWait();
+			return links;
+			
+		}
+
+	
 
 
-	        StackPane root = new StackPane();
-	        root.getChildren().add(dragTarget);
-
-	        Scene scene = new Scene(root, 500, 250);
-
-	        primaryStage.setTitle("Drag Test");
-	        primaryStage.setScene(scene);
-	        primaryStage.show();
-	    }
-
-	    /**
-	     * @param args the command line arguments
-	     */
-	    public static void main(String[] args) {
-	        launch(args);
-	    }
+	   
 
 	}
 
