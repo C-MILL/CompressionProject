a40746f4ea15dac6724032ad89e8e0e922afe1c0
atorha <chrigu_m@hotmail.com>
2018-12-03 20:31:21 +0100

    corrected

--- utils/DragAndDrop/DragAndDropController.java
+++ utils/DragAndDrop/DragAndDropController.java
@@ -1,36 +1,29 @@
 package DragAndDrop;
-
-
+ import Main.Main;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.input.DragEvent;
 import javafx.scene.input.Dragboard;
 import javafx.scene.input.TransferMode;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
-
-public class DragAndDropController {
+import Main.Main;
+ public class DragAndDropController {
 	//views
 	@FXML private Rectangle DragAndDropArea;
 	@FXML private Label labelNumberToCompress;
 	@FXML private Button DragAndDropButtonCancel;
-	private String[] links;
-
-	
-
-	public DragAndDropMain main;
-
-
-	public void setMain(DragAndDropMain dragAndDropMain) {
-		this.main=dragAndDropMain;
-
-	}
-
-	@FXML
+	private String[] links = new String[0];
+ 	public Main main;
+ 	public void setMain(Main main) {
+		this.main=main;
+ 	}
+ 	@FXML
 	private void handleDragDropped(DragEvent event) {
-
-		Dragboard DragboardLinks = event.getDragboard();	//Save all links in DragBoard
+ 		Dragboard DragboardLinks = event.getDragboard();	//Save all links in DragBoard
 		String somelinks = DragboardLinks.getFiles().toString();		//convert dragboard to string
 		somelinks= somelinks.substring(1, (somelinks.length()-1));		//substract beginning and end (rechteckige klammern)
 		String[] somelinksarray = somelinks.split(", ");		//split the links 
@@ -40,46 +33,30 @@ public class DragAndDropController {
 		for(int i=0; i<sublinks.length; i++) {
 			links[i]=sublinks[i];
 		}	//write old links into the new array
+		
 		for(int i=links.length-1; i<(links.length+somelinksarray.length-1); i++) {
 			for(int j=0; j<somelinksarray.length; j++) {
 				links[i]=somelinksarray[j];
-				System.out.println(links[i]);
 			} //write new links into the array
-			for(int h=0; h<links.length; h++) {
-				System.out.println(links[h]);
-					}
+			for(int j=0; j<links.length; j++) {
+				System.out.println(links[j]);
+			}
 		}
+	
 		
 	}
-
-	@FXML
+ 	@FXML
 	private void setLabelNumberToCompress(String number) {
 		System.out.println(number);
-
-	}
-
-	@FXML
+ 	}
+ 	@FXML
 	private void handleOnDragOver(DragEvent event) {
 		if(event.getDragboard().hasFiles()) {
 			event.acceptTransferModes(TransferMode.ANY);
 		}
-
-
-	}
-
-	@FXML
+ 	}
+ 	@FXML
 	private void handleDragAndDropButtonCancel() {
-	    Stage stage = (Stage) DragAndDropButtonCancel.getScene().getWindow();
-	    stage.close();
-	}
-	
-	
-	public String[] getLinks() {
-		return links;
-	}
-
-	
-	public void setLinks(String[] links) {
-		this.links = links;
+		Platform.exit();
 	}
 }
--- utils/Main/Main.java
+++ utils/Main/Main.java
@@ -1,30 +1,58 @@
 package Main;
-
+ import java.io.IOException;
+ import DragAndDrop.DragAndDropController;
+import Main.MainWindowController;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
-
-
-
-public class Main extends Application {
-
-
-
-	DragAndDrop.DragAndDropMain DADO=new DragAndDrop.DragAndDropMain();
-	
-	
-
-	@Override
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+ public class Main extends Application {
+ 	private Stage primaryStage;
+ 	@Override
 	public void start(Stage primaryStage) {
-		DADO.DragAndDropPopUpWindow();
-		String[] links=DADO.getLinks();
-		//System.out.println(links[0]);
+		this.primaryStage = new Stage();
+		DragAndDropPopUpWindow();
+		//mainWindow();
 	}
-
-
-	public static void main(String[] args) {
+ 	public void mainWindow() {
+		try {
+			FXMLLoader loader = new FXMLLoader(Main.class.getResource("MainWindow.fxml"));
+			AnchorPane pane = loader.load();
+			primaryStage.setMinHeight(400.00);
+			primaryStage.setMinWidth(500.00);
+			MainWindowController mainWindowController = loader.getController();
+			mainWindowController.setMain(this);
+			Scene scene=new Scene(pane);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+ 	}
+	
+	public void DragAndDropPopUpWindow() {
+		try {
+			FXMLLoader loader= new FXMLLoader(getClass().getClassLoader().getResource("\\DragAndDrop\\DragAndDropView.fxml"));
+			/*FXMLLoader loader = new FXMLLoader(Main.class.getResource("\\DragAndDrop\\DragAndDropView.fxml"));*/
+			AnchorPane pane = loader.load();
+			primaryStage.setMinHeight(400.00);
+			primaryStage.setMinWidth(500.00);
+			DragAndDropController DragAndDropController = loader.getController();
+			DragAndDropController.setMain(this);
+			Scene scene=new Scene(pane);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+ 		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+ 	}
+ 	public static void main(String[] args) {
 		launch(args);
 	}
-
-
 }
-
