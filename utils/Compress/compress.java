8ae8ffb93a37d52fb1cdea5f7e5934645b559dda
kruesin <kruesin@WS104620.loe.dhbw-loerrach.de>
2018-12-14 10:25:04 +0100

    compression works now: creates new file after the original filename. Images are always converted to "originalname+compr.jpeg"

--- utils/Compress/compress.java
+++ utils/Compress/compress.java
@@ -11,67 +11,61 @@ import javax.swing.ImageIcon;
 
 public class compress {
 
-	
-	//Constructor 1
-	public compress(String linkOfFile) {
-		try {
-			
-			//RANDOM TEXT
-			//Load Image
-		Image image = new ImageIcon(ImageIO.read(new File(linkOfFile))).getImage();
+	//Constructor 1 -> Default program functionality
+	public compress(String linkOfFile, String linkOfNewFolder) {
 
-		// creates new Folder if not already existing
-		//new File("/linkOfFile/Compressed_Images").mkdir();
-		
-		//Get resolution of image and divide trough 2
-		int newheight=(image.getHeight(null))/2;
-		int wewWidth = (image.getWidth(null))/2;
-		
-		//Compress
-		Image scaledImage = image.getScaledInstance(wewWidth, newheight,Image.SCALE_SMOOTH);
-		BufferedImage outImg = new BufferedImage(wewWidth, newheight,BufferedImage.TYPE_INT_RGB);
-		
-		//Does some stuff 
-		Graphics g = outImg.getGraphics();
-		g.drawImage(scaledImage, 0, 0, null);
-		g.dispose();
-		
-		//Writes new image to new folder link with the old name of the origin file...
-		//But I have no idea how you need to rearrange the link where it saves the new file...
-		//Good luck ;)
-		//
-		/* 
-		*	Nicolas 09.12.2018:
-		*	I think it could go in that direction... 
-		*	Hoping that "image" represents the current image name 
-		*	it should save it to the newly created folder inside the images folder.
-		 */
-		ImageIO.write(outImg, "jpeg", new File("C:/Users/chrig/Desktop/Beispiel_small.jpg"));
+		// *** for testing purposes: String linkOfFile = "C:/Users/kruesin/Downloads/1.jpg";
+
+		// initialize Image
+		Image image;
+		try {
+			// reads image from path input
+			image = new ImageIcon(ImageIO.read(new File(linkOfFile))).getImage();
+			// gets file path and saves it to f (important for further file processing)
+			File f = new File(linkOfFile);
+			// Extracts file name from file path
+			String originalImageName = f.getName();
+
+			// Removes file extension (.jpg / .jpeg / etc) -> important so that the newly created images all have the same file extension.
+			int pos = originalImageName.lastIndexOf(".");
+			String onlyImageName = originalImageName.substring(0, pos);
+
+			// Creates the image name for the exported, compressed image
+			String newImageName = onlyImageName + "_compr";
+
+			// *** for testing purposes: System.out.println(newImageName);
+
+			// creates new Folder if not already existing
+			//new File("/linkOfFile/Compressed_Images").mkdir();
+
+			//Get resolution of image and divide trough 2
+			int newheight=(image.getHeight(null))/2;
+			int wewWidth = (image.getWidth(null))/2;
+
+			//Compress
+			Image scaledImage = image.getScaledInstance(wewWidth, newheight,Image.SCALE_SMOOTH);
+			BufferedImage outImg = new BufferedImage(wewWidth, newheight,BufferedImage.TYPE_INT_RGB);
+
+			//Does some stuff 
+			Graphics g = outImg.getGraphics();
+			g.drawImage(scaledImage, 0, 0, null);
+			g.dispose();
+
+			// *** for testing purposes: ImageIO.write(outImg, "jpeg", new File("C:/Users/kruesin/Downloads/" + newImageName + ".jpeg"));
+
+			ImageIO.write(outImg, "jpeg", new File(linkOfNewFolder + newImageName + ".jpeg"));
 		} catch (IOException e) {
 			e.printStackTrace();
-		}
-	}
-	
-	///Compressed_Images +image+".jpg"
-	
-	//Constructor 2
+		}}
+
+	//Constructor 2 for extended functionality
 	public compress(String linkOfFile, String linkOfNewFolder, int width, int height) {
-		
-
-		
-
-	}
-	
-	
-	//Constructor 3
-public compress(String linkOfFile, String linkOfNewFolder, int maxSizeOfFile) {
-		
-
-		
 
 	}
 
+	//Constructor 3 for extended functionality
+	public compress(String linkOfFile, String linkOfNewFolder, int maxSizeOfFile) {
 
-
+	}
 
 }
