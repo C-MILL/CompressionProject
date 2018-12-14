538bd60636eb2e75b71f59d969e7264c5af9058f
Christof Müller <chrigu_m@hotmail.com>
2018-12-14 11:22:24 +0100

    NewestVersionOfProject

--- utils/directory/createNewFolder.java
+++ utils/directory/createNewFolder.java
@@ -1,5 +1,33 @@
 package directory;
 
+import java.io.File;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+
 public class createNewFolder {
+
+	public String createNewFolder() {
+		String linkOfNewFolder = null;
+
+		try {
+
+			// Gets todays Date
+			DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy.MM.dd");
+			LocalDateTime today = LocalDateTime.now();
+
+			
+			// Gets users home directory and splits it at Desktop
+			// Creates new folder with e.g. following path: C:\Users\mkaun\Desktop\Compressed 2018.12.14
+			String path = System.getProperty("user.home") + File.separator + "Desktop";
+			path += File.separator + "Compressed Images " + dtf.format(today);
+			File customDir = new File(path);
+			customDir.mkdirs();
+			linkOfNewFolder = path.replace("\\","/");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return linkOfNewFolder;
+	}
 
 }
