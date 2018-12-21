c648705e15ee4f2801e641eca5c6a0a5fc012b61
Christof Müller <chrigu_m@hotmail.com>
2018-12-21 16:38:02 +0100

    Hier ist eine Überarbeitung der Test Klasse die die Auflösung testet.

--- src/test/java/JUnitTesting/TestCompress.java
+++ src/test/java/JUnitTesting/TestCompress.java
@@ -2,7 +2,9 @@ package JUnitTesting;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
@@ -14,16 +16,42 @@ import org.junit.jupiter.api.Test;
 import Compress.compress;
 
 class TestCompress {
+	String linkOfSmallFile="./src/test/resources/testFileSmall.jpeg";
+	String linkOfSmallCompressedFile="./src/test/resources/testCompressed/testFileSmall_compr.jpeg";
+	String linkOfBigFile="./src/test/resources/testFileBig.jpg";
+	String linkOfBigCompressedFile="./src/test/resources/testCompressed/testFileBig_compr.jpeg";
+
+	
+
+	Image smallImage;
+	Image smallCompressedImage;
+	Image bigImage;
+	Image bigCompressedImage;
 
 	@Test
-	public void testCompressResolutionResizing() throws IOException { 
-		new compress("./src/test/resources/testFileSmall.jpg", "./src/test/resources/testCompressed");
-		Image uncompressedImage = new ImageIcon(ImageIO.read(new File("./src/test/resources/testFileSmall.jpg"))).getImage();
-		Image compressedImage = new ImageIcon(ImageIO.read(new File("./src/test/testFileSmall_compr.jpg"))).getImage();
-		
-
-		assertEquals(compressedImage.getHeight(null), uncompressedImage.getHeight(null), "blablabla");
+	public void testCompressResolutionResizingToSmallResolution() throws IOException { 
+		try {
+			new compress(linkOfSmallFile, "./src/test/resources/testCompressed");
+			smallImage= new ImageIcon(ImageIO.read(new File(linkOfSmallFile))).getImage();
+			smallCompressedImage  = new ImageIcon(ImageIO.read(new File(linkOfSmallCompressedFile))).getImage();
+			assertEquals(smallImage.getHeight(null), smallCompressedImage.getHeight(null), "The Small Images did get compress, even though they sould not get compressed.");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-
+	
+	
+	@Test
+	public void testCompressResolutionResizingBigFile() throws IOException { 
+		try {
+			new compress(linkOfBigFile, "./src/test/resources/testCompressed");
+			bigImage= new ImageIcon(ImageIO.read(new File(linkOfBigFile))).getImage();
+			bigCompressedImage  = new ImageIcon(ImageIO.read(new File(linkOfBigCompressedFile))).getImage();
+			assertEquals(bigImage.getHeight(null)/2, bigCompressedImage.getHeight(null), "The Small Images did get compress, even though they sould not get compressed.");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
+
 
